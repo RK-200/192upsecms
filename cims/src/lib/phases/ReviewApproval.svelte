53559b1dd3877eb6c0109c9b83317cb9d8ffdf7d
Winter129 <hikari.weeb120@gmail.com>
2026-02-09 12:50:13 +0800

    adjusted Review and approval add files

--- cims/src/lib/phases/ReviewApproval.svelte
+++ cims/src/lib/phases/ReviewApproval.svelte
@@ -48,34 +48,29 @@
 
 <hr />
 
-<h3>Add Files</h3>
+<h3>Add Default Files</h3>
 
-<div>
-  Drag & Drop or <u>Choose file</u> to upload
-</div>
+  <div style="border: 2px dashed #999; padding: 20px; text-align: center;">
+    Drag & Drop or <u>Choose file</u> to upload
+    <div style="font-size: 12px; color: #666;">
+      fig, zip, pdf, png, jpeg
+    </div>
+  </div>
 
-<p>fig, zip, pdf, png, jpeg</p>
+  <p style="text-align: center; color: #999;">OR</p>
 
-<p>OR</p>
+  <input
+    type="text"
+    placeholder="Add file URL"
+    style="width: 100%; margin-bottom: 8px;"
+  />
 
-<input
-  type="text"
-  placeholder="Add file URL"
-  bind:value={fileUrl}
-/>
+  <button type="button">
+    Upload
+  </button>
 
-<button type="button">
-  Upload
-</button>
-
-<br /><br />
-
-<button type="button">
-  Cancel
-</button>
-
-<button type="button">
-  Import
-</button>
-
-</div>
+  <div style="margin-top: 16px; text-align: right;">
+    <button type="button">Cancel</button>
+    <button type="button">Import</button>
+  </div>
+  </div>
