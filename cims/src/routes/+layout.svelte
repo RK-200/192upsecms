dea751ffbe6be48beed914071da3ac0bb23bb4c0
Lejaaand <91518237+Lejaaand@users.noreply.github.com>
2026-02-10 16:13:06 +0800

    added header style

--- cims/src/routes/+layout.svelte
+++ cims/src/routes/+layout.svelte
@@ -1,18 +1,100 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import upseLogo from '$lib/assets/UPSE CMS logo.png'; 
+	import { Workflow, FilePlusCorner, Search } from 'lucide-svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
+	<title>UPSE Contracts Management System</title>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-<nav>
-	<a href="/">upse image</a>
-	<a href="/workflow">Create Workflow</a>
-	<a href="/contract">Create Contract</a>
-	<a href="/view"> View Contracts</a>
-</nav>
+<header class="header-container">
+	<div class="logo-section">
+		<a href="/">
+			<img src={upseLogo} alt="UPSE CMS Logo" class="logo-img" />
+		</a>
+	</div>
 
-{@render children()}
+	<nav class="nav-links">
+		<a href="/workflow" class="nav-btn">
+			<Workflow size={24} strokeWidth={2.5} />
+			<span class="nav-btn-txt">Edit Workflows</span>
+		</a>
+		
+		<a href="/contract" class="nav-btn">
+			<FilePlusCorner size={24} strokeWidth={2.5} />
+			<span class="nav-btn-txt">Create Contract</span>
+		</a>
+		
+		<a href="/view" class="nav-btn">
+			<Search size={24} strokeWidth={2.5} />
+			<span class="nav-btn-txt">View Contracts</span>
+		</a>
+	</nav>
+</header>
+
+<main>
+	{@render children()}
+</main>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap');
+
+    :global(body) {
+        font-family: 'Poppins', sans-serif;
+        margin: 0;
+    }
+	.header-container {
+		display: flex;
+		align-items: center; 
+		justify-content: space-between;
+		padding: 0.75rem 2.5rem;
+		background-color: white;
+		border-bottom: 1px solid #e5e7eb;
+		box-shadow: 0 1px 3px rgba(0,0,0,0.1);
+	}
+
+	.logo-img {
+		height: 80px;
+		width: auto;
+		display: block;
+	}
+
+	.nav-links {
+		display: flex;
+		flex-direction: row;
+		gap: 12px;
+		align-items: center;
+	}
+
+	.nav-btn {
+		display: flex;
+		align-items: center;
+		gap: 8px;
+		padding: 8px 20px;
+		background-color: #7B1113;
+		color: white;
+		border-radius: 9999px;
+		text-decoration: none;
+		font-weight: 600;
+		font-size: 13px;
+		white-space: nowrap;
+		transition: background-color 0.2s, transform 0.1s;
+		box-shadow: 0 3px 6px rgba(0,0,0,0.1);
+	}
+
+	.nav-btn-txt {
+		font-size: 18px;
+	}
+
+	.nav-btn:hover {
+		background-color: #5a0c0e;
+	}
+
+	.nav-btn:active {
+		transform: scale(0.96);
+	}
+</style>
